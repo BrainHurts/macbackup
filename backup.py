7e604fcc18b94842d94a864247cdae38cc46777f
Cary Kinsfather <cary.kinsfather@gmail.com>
2021-04-06 10:11:33 -0500

    cleaned up variable names to make more sense, added refresh, browse, and exit button

--- backup.py
+++ backup.py
@@ -1,16 +1,12 @@
 import shutil
 import os
 from tkinter import *
+from tkinter import filedialog
 from elevate import elevate
+import time
 
 # Request Sudo Rights to Access Files for Backup
 elevate(graphical=False)
-
-# Locate USB
-volume_list = os.listdir('/Volumes')
-
-# Get user accounts from iMac
-users = os.listdir('/Users')
 
 
 def grabfiles(theuser, finalvol):
@@ -66,50 +62,74 @@ def grabfiles(theuser, finalvol):
     os.system("""open '""" + full_cur_dir_path + """'""")
 
     listbox_vol.configure(selectbackground="#18a104")
-    listbox.configure(selectbackground="#18a104")
+    listbox_user.configure(selectbackground="#18a104")
 
+# Set variables to empty to check is variable picked later on backup click.
+picked_user = ''
+picked_vol = ''
 
-# Execute backup fuction
-
+# Building the tkinter window as well as some functions for the buttons.
 window = Tk()
 
 window.title("NBackup")
 window.resizable(width=False, height=True)
 
-def clicked():
+def backup_clicked():
+    if picked_vol == '':
+        listbox_vol.configure(background="#f54242")
+        return None
+    if picked_user == '':
+        listbox_user.configure(background="#f54242")
+        return None
     grabfiles(picked_user, picked_vol)
 
-label = Label(window, text="Backup Location")
-labeluser = Label(window, text="User Folder")
+
+def browse_clicked():
+    global picked_vol
+    folder_selected = filedialog.askdirectory()
+    picked_vol = folder_selected
+    label_vol.configure(text="Backup Location: " + picked_vol)
+    # listbox_vol.insert(0, picked_vol)
+    print(picked_vol)
+    return picked_vol
+
+
+def refresh_clicked():
+    get_users_volumes()
+
+
+# ----------- Create objects
+label_vol = Label(window, text="Backup Location")
+label_user = Label(window, text="User Folder")
+
 listbox_vol = Listbox(window, width=45, selectmode=SINGLE, bd=0, bg='#222', fg='#fff', selectforeground='#fff', selectbackground="#064fd6", activestyle='none', exportselection=False)
-listbox = Listbox(window, width=45, selectmode=SINGLE, bd=0, bg='#222', fg='#fff', selectforeground='#fff', selectbackground="#064fd6", activestyle='none')
-button = Button(window, text="Backup", width=30, height=2, command=clicked)
+listbox_user = Listbox(window, width=45, selectmode=SINGLE, bd=0, bg='#222', fg='#fff', selectforeground='#fff', selectbackground="#064fd6", activestyle='none')
+
+browse_button = Button(window, text="Browse to Folder", width=30, height=2, command=browse_clicked)
+refresh_button = Button(window, text="Refresh", width=30, height=2, command=refresh_clicked)
+backup_button = Button(window, text="Backup", width=30, height=2, command=backup_clicked)
+exit_button = Button(window, text="Exit", width=30, height=2, command=exit)
+
+
 documents_label = Label(text='Documents Count:')
 downloads_label = Label(text='Downloads Count:')
 desktop_label = Label(text='Desktop Count:')
 
-label.grid(column=0, columnspan=1, row=0, padx=5, ipadx=5, ipady=5)
-labeluser.grid(column=1, columnspan=1, row=0, padx=5, ipadx=5, ipady=5)
+# ----------- Pack it in
+label_vol.grid(column=0, columnspan=1, row=0, padx=5, ipadx=5, ipady=5)
+label_user.grid(column=1, columnspan=1, row=0, padx=5, ipadx=5, ipady=5)
+
 listbox_vol.grid(column=0, row=1, padx=5, ipadx=5, ipady=5)
-listbox.grid(column=1, row=1, padx=5, pady=5, ipadx=5, ipady=5)
-button.grid(column=0, columnspan=1, row=3, rowspan=3, padx=5, pady=5)
-documents_label.grid(column=1, row=3)
-downloads_label.grid(column=1, row=4)
-desktop_label.grid(column=1, row=5)
+listbox_user.grid(column=1, row=1, padx=5, pady=5, ipadx=5, ipady=5)
 
-# Add Volumes to Listbox_vol
-listbox_vol.delete(0, 'end')
-i = 0
-for vol in volume_list:
-    listbox_vol.insert(i, vol)
-    i = i + 1
+browse_button.grid(column=1, columnspan=1, row=3, rowspan=1, padx=5, pady=5)
+refresh_button.grid(column=1, columnspan=1, row=4, rowspan=1, padx=5, pady=5)
+backup_button.grid(column=1, columnspan=1, row=5, rowspan=1, padx=5, pady=5)
+exit_button.grid(column=1, columnspan=1, row=6, rowspan=1, padx=5, pady=5)
 
-# Add User Accounts to Listbox
-listbox.delete(0, 'end')
-i = 0
-for user in users:
-    listbox.insert(i, user)
-    i = i + 1
+documents_label.grid(column=0, row=3)
+downloads_label.grid(column=0, row=4)
+desktop_label.grid(column=0, row=5)
 
 
 def callbackVol(event):
@@ -118,10 +138,10 @@ def callbackVol(event):
     if selection:
         index = selection[0]
         picked_vol = event.widget.get(index)
-        listbox_vol.configure(selectbackground="#064fd6")
+        listbox_vol.configure(selectbackground="#064fd6", background="#222")
         print(picked_vol)
         picked_vol = "/Volumes/" + picked_vol
-        label.configure(text="Backup Location: " + picked_vol)
+        label_vol.configure(text="Backup Location: " + picked_vol)
         return picked_vol
 
 
@@ -131,8 +151,8 @@ def callbackName(event):
     if selection:
         index = selection[0]
         picked_user = event.widget.get(index)
-        listbox.configure(selectbackground="#064fd6")
-        labeluser.configure(text="User Folder: " + picked_user)
+        listbox_user.configure(selectbackground="#064fd6", background="#222")
+        label_user.configure(text="User Folder: " + picked_user)
         print(picked_user)
 
         # Count Files On Documents
@@ -158,8 +178,35 @@ def callbackName(event):
         return(picked_user)
 
 
+def get_users_volumes():
+    # Locate USB
+    volume_list = os.listdir('/Volumes')
+
+    # Get user accounts from iMac
+    users = os.listdir('/Users')
+
+    # Add Volumes to Listbox_vol
+    listbox_vol.delete(0, 'end')
+    i = 0
+    for vol in volume_list:
+        listbox_vol.insert(i, vol)
+        i = i + 1
+
+    # Add User Accounts to Listbox
+    listbox_user.delete(0, 'end')
+    i = 0
+    for user in users:
+        listbox_user.insert(i, user)
+        i = i + 1
+
+
+get_users_volumes()
+
+
+folderPath = StringVar()
+
 listbox_vol.bind("<<ListboxSelect>>", callbackVol)
 
-listbox.bind("<<ListboxSelect>>", callbackName)
+listbox_user.bind("<<ListboxSelect>>", callbackName)
 # window.configure(background="#000")
 window.mainloop()
